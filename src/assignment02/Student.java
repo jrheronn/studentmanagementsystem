69076c0deb2911cf034aca6ecf1e618b4488a6e3
jrheronn <jrhernn@proton.me>
2023-09-25 16:39:10 -0500

    Initial commit

---
+++ src/assignment02/Student.java
@@ -0,0 +1,15 @@
+package assignment02;
+
+public class Student {
+    String name;
+    String studentID;
+    String email;
+    String address;
+
+    public Student(String name, String studentID, String email, String address) {
+        this.name = name;
+        this.studentID = studentID;
+        this.email = email;
+        this.address = address;
+    }
+}
