9b088542e1e42ac624963f82db781fe2c8556199
jrheronn <jrhernn@proton.me>
2023-10-03 17:00:09 -0500

    Had reader fill linked list instead of trying to pull from arrays

--- src/assignment02/DataReader.java
+++ src/assignment02/DataReader.java
@@ -3,7 +3,6 @@ package assignment02;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 
 public class DataReader {
     private String inputFile;
@@ -12,53 +11,41 @@ public class DataReader {
         this.inputFile = inputFile;
     }
 
-    public ArrayList<Course> readCourses() throws IOException {
-        ArrayList<Course> courses = new ArrayList<>();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile))) {
+    // Method to read data from inputFile.txt and fill both linked lists
+    public void readData(CourseDoublyLinkedList courseList, StudentSinglyLinkedList studentList) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.startsWith("Course Number")) {
+            boolean categoriesLine = true;
+
+            while ((line = reader.readLine()) != null) {
+                if (categoriesLine) {
+                    // Skip the categories line
+                    categoriesLine = false;
                     continue;
                 }
-                String[] categories = line.split("\t");
 
-                // Checks if the line has both Course Number and Course Name
-                if (categories.length >= 2) {
-                    String courseNumber = categories[0].trim();
-                    String courseName = categories[1].trim();
+                String[] data = line.split("\t");
 
+                if (data.length == 6) {
+                    // Course Number, Course Name, Student Name, Student ID, Email, Address
+                    String courseNumber = data[0].trim();
+                    String courseName = data[1].trim();
+                    String studentName = data[2].trim();
+                    String studentID = data[3].trim();
+                    String email = data[4].trim();
+                    String address = data[5].trim();
+
+                    // Create and fill Course Doubly Linked List
                     Course course = new Course(courseNumber, courseName);
-                    courses.add(course);
-                }
-            }
-        }
+                    courseList.addCourse(course);
 
-        return courses;
-    }
-
-    public ArrayList<Student> readStudents() throws IOException {
-        ArrayList<Student> students = new ArrayList<>();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.startsWith("Course Number")) {
-                    continue;
-                }
-                String[] categories = line.split("\t");
-
-                if (categories.length >= 6) {
-                    String studentName = categories[2].trim();
-                    String studentID = categories[3].trim();
-                    String email = categories[4].trim();
-                    String address = categories[5].trim();
-
+                    //Create and fill the Student Singly Linked List
                     Student student = new Student(studentName, studentID, email, address);
-                    students.add(student);
+                    studentList.addStudent(student);
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return students;
     }
 }
