97d1395a2f63e063ce34c3832e5033988eae3f0f
jrheronn <jrhernn@proton.me>
2023-10-03 11:58:30 -0500

    Created FileReader class to read input file

---
+++ src/assignment02/DataReader.java
@@ -0,0 +1,22 @@
+package assignment02;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class DataReader {
+    private String inputFile;
+
+    public DataReader(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public ArrayList<Course> readCourses() throws IOException {
+
+    }
+
+    public ArrayList<Student> readStudents() throws IOException {
+
+    }
+}
